0f9f3ceac5399f311cf393733389188599026afd
Gavyn Ezell <72951570+gavyn-ezell@users.noreply.github.com>
2022-11-27 22:33:08 -0800

    query fix, more info grabbing, some style

--- source/scripts/queries.js
+++ source/scripts/queries.js
@@ -1,38 +1,56 @@
 // Query Functions for site functionality
 
-
 /*
-*      query_player_tournaments
+*  One giant query that gets player's recent placements, upcoming tournies, and socials
 *
-*      Performs QUERY FOR PLAYER'S RECENT + UPCOMING TOURNAMENTS using fetch
-*      
-*      Variables:
-*      playerId: ID of player we want to query
-*      
-*      
 */
-export async function query_player_tournaments(playerId) {
-  //set up query for fetch()
-  let query_player_tournaments = `
+export async function query_playercard_info(playerId) {
+
+  //setting up the query
+  let query_playercard_info = `
   query ($playerId: ID!) {
-    player (id: $playerId) {
+    player(id: $playerId) {
+      recentStandings(videogameId: 1386, limit: 3) {
+            placement
+            entrant {
+              event {
+                name
+                numEntrants
+                tournament {
+                  name
+                }
+              }
+            }
+          } 
       user {
+        location {
+          country
+        }
         tournaments (query: {perPage: 8, page: 1}) {
-          nodes {
-            name
-            slug
-            id
-            numAttendees
-            countryCode
-            startAt
-          }
+            nodes {
+              name
+              slug
+              id
+              numAttendees
+              countryCode
+              startAt
+            }
+        }
+        authorizations {
+          externalUsername
+          type
+          url
+        }
+        images {
+          height
+          url
+          width
+          type
         }
       }
     }
   }
   `;
-
-  //now we can perform the fetch request
 
   let result = await fetch('https://api.start.gg/gql/alpha', {
             method: 'POST',
@@ -42,7 +60,7 @@ export async function query_player_tournaments(playerId) {
                 'Accept': 'application/json',
             },
             body: JSON.stringify({
-                'query': query_player_tournaments,
+                'query': query_playercard_info,
                 'variables': {
                   "playerId": playerId
                 }
@@ -53,60 +71,6 @@ export async function query_player_tournaments(playerId) {
         }).then(data => {
             return data;
         });
-  return result;
-}
-
-/*
-*      query_player_standing
-*
-*      Performs QUERY FOR PLAYER'S 3 recent placements at tournaments
-*      
-*      Variables:
-*      playerId: ID of player we want to query
-*      
-*      
-*/
-
-export async function query_player_standings(playerId) {
-
-    let query_player_standings = `
-    query ($playerId: ID!) {
-      player(id: $playerId) {
-        recentStandings(videogameId: 1386, limit: 3) {
-          placement
-          entrant {
-            event {
-              name
-              numEntrants
-              tournament {
-                name
-              }
-            }
-          }
-        } 
-        gamerTag
-      }
-    }`;
-
-    let result = await fetch('https://api.start.gg/gql/alpha', {
-            method: 'POST',
-            headers: {
-                'Authorization': 'Bearer c7ee5b396ab4bae2c39f64ac532c686b',
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            },
-            body: JSON.stringify({
-                'query': query_player_standings,
-                'variables': {
-                  "playerId": playerId
-                }
-            })
-
-        }).then(response => {
-            return response.json();
-        }).then(data => {
-            return data;
-        });
-  //console.log(result);
+  console.log(result);
   return result;
 }
